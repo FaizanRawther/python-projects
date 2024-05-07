84d1ac8836bb2198b782945934d7153e78498f01
FaizanRawther <107398300+FaizanRawther@users.noreply.github.com>
2024-05-07 20:44:59 +0530

    Delete children stats directory

--- children stats/analysis.py
+++
@@ -1,8 +0,0 @@
-#to analyse in 5 min
-import pandas as pd
-from pandas_profiling import ProfileReport
-df1=pd.read_excel("children_stats_only (2).xlsx")
-print(df1)
-#generate a report
-profile=ProfileReport(df1)
-profile.to_file(output_file="childrenStats.html")
